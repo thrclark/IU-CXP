b18fd44d0055b62b76f367418888afad248b8df3
thrclark <thrclark@iu.edu>
2026-09-02 11:53:03 -0400

    updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,18 @@ import { Routes } from '@angular/router';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { NotificationCenterComponent } from './notification-center/notification-center.component';
 import { PlaceholderPageComponent } from './placeholder-page/placeholder-page.component';
+import { MapsPageComponent } from './maps-page/maps-page.component';
+import { ServiceDetailComponent } from './service-detail/service-detail.component';
 
 export const routes: Routes = [
   { path: '', component: HelloWorldComponent },
   { path: 'dashboard', component: HelloWorldComponent },
   { path: 'notifications', component: NotificationCenterComponent },
+  { path: 'services/:slug', component: ServiceDetailComponent },
   { path: 'notifications/settings/general', component: PlaceholderPageComponent, data: { title: 'General Settings', icon: 'rvt-gear' } },
   { path: 'notifications/settings/notifications', component: PlaceholderPageComponent, data: { title: 'Notification Settings', icon: 'rvt-gear' } },
   { path: 'announcements', component: PlaceholderPageComponent, data: { title: 'Announcements', icon: 'rvt-megaphone' } },
   { path: 'events-calendars', component: PlaceholderPageComponent, data: { title: 'Events & Calendars', icon: 'rvt-calendar' } },
   { path: 'directory', component: PlaceholderPageComponent, data: { title: 'Directory', icon: 'address-book' } },
-  { path: 'maps', component: PlaceholderPageComponent, data: { title: 'Maps', icon: 'rvt-map-pin' } },
+  { path: 'maps', component: MapsPageComponent },
 ];
